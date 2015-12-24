5b252b92f4a58b00af76c4cebee5519e386c8966
ghost-dog <ghost-dog@yandex.ru>
2015-12-25 01:12:34 +0500

    seventh day 5

--- src/t7.go
+++ src/t7.go
@@ -16,8 +16,13 @@ func main() {
 	// var inputReader *bufio.Reader
 	// var inputString string
 	var prog [340][5]string
+	type node struct {
+		value int
+		check bool
+	}
+
 	//var sign map[string]int
-	sign := make(map[string][2]int)
+	sign := make(map[string]node)
 
 	inputFile, inputError := os.Open("adv7.txt")
 
@@ -42,29 +47,56 @@ func main() {
 		}
 		//fmt.Printf("The input was: %s", inputString)
 
-		j = i
 		if _, err := fmt.Sscanf(inputString, "%s %s %s -> %s", &prog[i][0], &prog[i][1], &prog[i][2], &prog[i][3]); err == nil {
 			//fmt.Printf("%s %s %s -> %s\n", prog[i][0], prog[i][1], prog[i][2], prog[i][3])
+			if n, err := strconv.Atoi(prog[i][0]); err == nil {
+				sign[prog[i][0]].value = n
+				sign[prog[i][0]].check = true
+			} else {
+				if _, found := sign[prog[i][0]]; !found {
+					sign[prog[i][0]].value = 0
+					sign[prog[i][0]].check = false
+				}
+			}
+			if n, err := strconv.Atoi(prog[i][2]); err == nil {
+				sign[prog[i][2]].value = n
+				sign[prog[i][2]].check = true
+			} else {
+				if _, found := sign[prog[i][2]]; !found {
+					sign[prog[i][2]].value = 0
+					sign[prog[i][2]].check = false
+				}
+			}
+			if _, found := sign[prog[i][3]]; !found {
+				sign[prog[i][3]].value = 0
+				sign[prog[i][3]].check = false
+			}
+
 			i++
 		} else if _, err := fmt.Sscanf(inputString, "%s %s -> %s", &prog[i][0], &prog[i][1], &prog[i][3]); err == nil {
-			//fmt.Printf("%s %s -> %s\n", prog[i][0], prog[i][1], prog[i][3])
+			if prog[i][0] == "NOT" {
+				if _, found := sign[prog[i][1]]; !found {
+					sign[prog[i][1]].value = 0
+					sign[prog[i][1]].check = false
+				}
+				if _, found := sign[prog[i][3]]; !found {
+					sign[prog[i][3]].value = 0
+					sign[prog[i][3]].check = false
+				}
+			} //fmt.Printf("%s %s -> %s\n", prog[i][0], prog[i][1], prog[i][3])
 			i++
 		} else if _, err := fmt.Sscanf(inputString, "%s -> %s", &prog[i][0], &prog[i][3]); err == nil {
 			//fmt.Printf("%s -> %s\n", prog[i][0], prog[i][3])
-			i++
-		}
 
-		// form map
-		if prog[j][0] == "NOT" {
-			sign[prog[j][1]] = append(sign[prog[j][1]], 0)
-			sign[prog[j][1]] = append(sign[prog[j][1]], 0)
-			sign[prog[j][3]] = 0
-		} else if n, err := strconv.Atoi(prog[j][0]); err == nil {
-			sign[prog[j][0]] = n
-			sign[prog[j][3]] = 0
-		} else {
-			sign[prog[j][0]] = 0
-			sign[prog[j][3]] = 0
+			if _, found := sign[prog[i][1]]; !found {
+				sign[prog[i][1]].value = 0
+				sign[prog[i][1]].check = false
+			}
+			if _, found := sign[prog[i][3]]; !found {
+				sign[prog[i][3]].value = 0
+				sign[prog[i][3]].check = false
+			}
+			i++
 		}
 
 	}
