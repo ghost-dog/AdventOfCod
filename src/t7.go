cf9468b433918812fcf62e711e989946b7121cf9
Sergey Malkov <ghost-dog@yandex.ru>
2015-12-30 18:00:41 +0500

    seventh day 4

--- src/t7.go
+++ src/t7.go
@@ -17,7 +17,8 @@ func main() {
 	// var inputString string
 
 	var prog [340][5]string
-	var prog_t [5]string
+	//var prog_t [5]string
+	words := map[string]string{"RSHIFT": "RSHIFT", "OR": "OR", "AND": "AND", "NOT": "NOT", "LSHIFT": "LSHIFT", "->": "->"}
 
 	type node struct {
 		value int
@@ -38,6 +39,8 @@ func main() {
 
 	inputReader := bufio.NewReader(inputFile)
 	i := 0
+	a := 0
+	b := 0
 	s := 0
 	//j := 0
 	exit := 0
@@ -45,55 +48,102 @@ func main() {
 		inputString, readerError := inputReader.ReadString('\n')
 		if readerError == io.EOF {
 			//fmt.Println("sign:", sign)
-			fmt.Printf("Total: %d %d", s, i)
+			//fmt.Printf("Total: %d %d", s, i)
 			//return // error or EOF
 			exit = 1
 		}
 		//fmt.Printf("The input was: %s", inputString)
 
-		if _, err := fmt.Sscanf(inputString, "%s %s %s %s %s", &prog_t[0], &prog_t[1], &prog_t[2], &prog_t[3], &prog_t[4]); err == nil {
+		if _, err := fmt.Sscanf(inputString, "%s %s %s %s %s", &prog[i][0], &prog[i][1], &prog[i][2], &prog[i][3], &prog[i][4]); err == nil {
 			//fmt.Printf("%s %s %s -> %s\n", prog[i][0], prog[i][1], prog[i][2], prog[i][3])
-			if prog_t[3] == "->" {
-
-			}
-
-			if n, err := strconv.Atoi(prog[i][0]); err == nil {
-				sign[prog[i][0]] = node{n, true}
-			} else {
+			if _, err := strconv.Atoi(prog[i][0]); err != nil {
 				if _, found := sign[prog[i][0]]; !found {
-					sign[prog[i][0]] = node{0, false}
+					if _, found := words[prog[i][0]]; !found {
+						sign[prog[i][0]] = node{0, false}
+					}
 				}
 			}
-			if n, err := strconv.Atoi(prog[i][2]); err == nil {
-				sign[prog[i][2]] = node{n, true}
-			} else {
-				if _, found := sign[prog[i][2]]; !found {
-					sign[prog[i][2]] = node{0, false}
-				}
-			}
-			if _, found := sign[prog[i][3]]; !found {
-				sign[prog[i][3]] = node{0, false}
-			}
-
-			i++
-		} else if _, err := fmt.Sscanf(inputString, "%s %s -> %s", &prog[i][0], &prog[i][1], &prog[i][3]); err == nil {
-			if prog[i][0] == "NOT" {
+			if _, err := strconv.Atoi(prog[i][1]); err != nil {
 				if _, found := sign[prog[i][1]]; !found {
-					sign[prog[i][1]] = node{0, false}
+					if _, found := words[prog[i][1]]; !found {
+						sign[prog[i][1]] = node{0, false}
+					}
 				}
-				if _, found := sign[prog[i][3]]; !found {
-					sign[prog[i][3]] = node{0, false}
-				}
-			} //fmt.Printf("%s %s -> %s\n", prog[i][0], prog[i][1], prog[i][3])
-			i++
-		} else if _, err := fmt.Sscanf(inputString, "%s -> %s", &prog[i][0], &prog[i][3]); err == nil {
-			//fmt.Printf("%s -> %s\n", prog[i][0], prog[i][3])
-
-			if _, found := sign[prog[i][1]]; !found {
-				sign[prog[i][1]] = node{0, false}
 			}
-			if _, found := sign[prog[i][3]]; !found {
-				sign[prog[i][3]] = node{0, false}
+			if _, err := strconv.Atoi(prog[i][2]); err != nil {
+				if _, found := sign[prog[i][2]]; !found {
+					if _, found := words[prog[i][2]]; !found {
+						sign[prog[i][2]] = node{0, false}
+					}
+				}
+			}
+			if _, err := strconv.Atoi(prog[i][3]); err != nil {
+				if _, found := sign[prog[i][3]]; !found {
+					if _, found := words[prog[i][3]]; !found {
+						sign[prog[i][3]] = node{0, false}
+					}
+				}
+			}
+			if _, err := strconv.Atoi(prog[i][4]); err != nil {
+				if _, found := sign[prog[i][4]]; !found {
+					if _, found := words[prog[i][4]]; !found {
+						sign[prog[i][4]] = node{0, false}
+					}
+				}
+			}
+
+			i++
+		} else if _, err := fmt.Sscanf(inputString, "%s %s %s %s", &prog[i][0], &prog[i][1], &prog[i][2], &prog[i][3]); err == nil {
+			if _, err := strconv.Atoi(prog[i][0]); err != nil {
+				if _, found := sign[prog[i][0]]; !found {
+					if _, found := words[prog[i][0]]; !found {
+						sign[prog[i][0]] = node{0, false}
+					}
+				}
+			}
+			if _, err := strconv.Atoi(prog[i][1]); err != nil {
+				if _, found := sign[prog[i][1]]; !found {
+					if _, found := words[prog[i][1]]; !found {
+						sign[prog[i][1]] = node{0, false}
+					}
+				}
+			}
+			if _, err := strconv.Atoi(prog[i][2]); err != nil {
+				if _, found := sign[prog[i][2]]; !found {
+					if _, found := words[prog[i][2]]; !found {
+						sign[prog[i][2]] = node{0, false}
+					}
+				}
+			}
+			if _, err := strconv.Atoi(prog[i][3]); err != nil {
+				if _, found := sign[prog[i][3]]; !found {
+					if _, found := words[prog[i][3]]; !found {
+						sign[prog[i][3]] = node{0, false}
+					}
+				}
+			}
+			i++
+		} else if _, err := fmt.Sscanf(inputString, "%s %s %s", &prog[i][0], &prog[i][1], &prog[i][2]); err == nil {
+			if _, err := strconv.Atoi(prog[i][0]); err != nil {
+				if _, found := sign[prog[i][0]]; !found {
+					if _, found := words[prog[i][0]]; !found {
+						sign[prog[i][0]] = node{0, false}
+					}
+				}
+			}
+			if _, err := strconv.Atoi(prog[i][1]); err != nil {
+				if _, found := sign[prog[i][1]]; !found {
+					if _, found := words[prog[i][1]]; !found {
+						sign[prog[i][1]] = node{0, false}
+					}
+				}
+			}
+			if _, err := strconv.Atoi(prog[i][2]); err != nil {
+				if _, found := sign[prog[i][2]]; !found {
+					if _, found := words[prog[i][2]]; !found {
+						sign[prog[i][2]] = node{0, false}
+					}
+				}
 			}
 			i++
 		}
@@ -102,9 +152,44 @@ func main() {
 	// parsing
 	exit = 0
 	for exit == 0 {
-		for i := 1; i < 340; i++ {
-			fmt.Printf("%s %s %s -> %s\n", prog[i][0], prog[i][1], prog[i][2], prog[i][3])
+		for i := 0; i < 340; i++ {
+			a = 0
+			al = false
+			b = 0
+			bl = false
+			s = 0
+			sl = false
+			//fmt.Printf("%s %s %s %s %s\n", prog[i][0], prog[i][1], prog[i][2], prog[i][3], prog[i][4])
+			if prog[i][1] == "->" {
+				if n, err := strconv.Atoi(prog[i][0]); err == nil {
+					a = n;
+					al = true;
+				else {
+					if nd, found := sign[prog[i][1]]; found {
+						if nd.check {
+							a = nd.value;
+							al = true;
+						}	
+					}	
+				}
+				if al {
+					if nd, found := sign[prog[i][2]]; found {
+							nd.value = a;
+							nd.check = true;
+							prog[i][5] = '1'
+					}	
+				}					}
+
+			} else if prog[i][2] == "->" {
+
+			} else if prog[i][3] == "->" {
+
+			}	
+			if n, err := strconv.Atoi(prog[i][0]); err == nil {
+
+			}
 		}
 		exit = 1
 	}
+	//fmt.Println(sign)
 }
